1e5797ecaa962373a08278c8f733b980def52675
Maarten Bressinck <maartenbressinck@MacBook-Pro-van-Maarten.local>
2014-04-09 10:25:37 +0200

    test

---
+++ www/services/uploadMediaNonNativeAdmin.php
@@ -0,0 +1,40 @@
+<?php // You need to add server side validation and better error handling here
+
+include('SimpleImage.php');
+$data = array();
+
+if(isset($_GET['files']))
+{
+	$error = false;
+	$files = array();
+	$id = uniqid();
+
+	$uploaddir =  $_SERVER['DOCUMENT_ROOT'] . "/common/uploads/";
+	foreach($_FILES as $file)
+	{
+
+
+		if(move_uploaded_file($file['tmp_name'], $uploaddir .basename(  $id . $file['name'])))
+		{		
+		   $files[] = $uploaddir . $id .$file['name'];
+		   $file_location = $uploaddir; # Image folder Path
+		   $image = new SimpleImage();
+		   $image->load($file_location. $id . $file['name']);
+      	   $image->resizeToHeight(300);
+		   $image->save('../common/uploads/'. $id .$file['name']);
+		}
+		else
+		{
+		    $error = true;
+		}
+	}
+	$data = ($error) ? array('error' => 'There was an error uploading your files') : array('files' => $files);
+}
+else
+{
+	$data = array('success' => 'Form was submitted', 'formData' => $_POST);
+}
+
+echo json_encode($data);
+
+?>
